661624c58254d9d9b60e1bef372dc0e3a45e0ecc
sdrrv <duartecelvas@gmail.com>
2022-11-23 23:07:29 +0000

    Template Portfolio reworked

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import ParticleBackground from "./assets/ParticleBackground";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const oldPort : string = "https://web.tecnico.ulisboa.pt/duartecelvas/";
 
   return (
+      <>
+          <ParticleBackground />
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Duarte Elvas</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        New portfolio still under development, to see the old one
       </p>
+      <div className="card">
+        <button onClick={() => window.location.href = oldPort}>
+          Click here
+        </button>
+      </div>
     </div>
+      </>
   )
 }
 
---
+++ src/assets/ParticleBackground.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Particles from "react-tsparticles";
+
+function ParticleBackground() {
+  return (
+    <Particles
+      className="particle-background"
+      params={{
+        particles: {
+          number: {
+            value: 20,
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+          },
+          color: {
+            value: "#ffffff",
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              color: "#000000",
+            },
+            polygon: {
+              nb_sides: 5,
+            },
+          },
+          opacity: {
+            value: 1,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0,
+              sync: false,
+            },
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 4,
+              size_min: 0.3,
+              sync: false,
+            },
+          },
+          move: {
+            enable: true,
+            speed: 1,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 600,
+            },
+          },
+        },
+      }}
+    />
+  );
+}
+
+export default ParticleBackground;
