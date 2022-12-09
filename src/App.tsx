a1290ef00a80b9d76e9c914273da70e51c8c830d
Duarte Elvas <duartecelvas@gmail.com>
2022-12-09 22:58:16 +0000

    Feature/typing (#2)
    * Typing added
    
    * Size and bold
    
    * Speed reajusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import ParticleBackground from "./assets/ParticleBackground";
+import ReactTypingEffect from "react-typing-effect";
 import "./App.scss";
 
 function App() {
   const oldPort = "https://web.tecnico.ulisboa.pt/duartecelvas/";
+  const titles = ["Software Engineer", "Developer", "Game Developer"];
 
   return (
     <>
       <ParticleBackground />
       <div className="App">
         <h1>Duarte Elvas</h1>
-        <p className="read-the-docs">
-          New portfolio still under development, to see the old one
+        <p className="description">
+          I am a{" "}
+          <b>
+            {" "}
+            <ReactTypingEffect
+              speed={100}
+              eraseSpeed={100}
+              typingDelay={1500}
+              eraseDelay={1500}
+              text={titles}
+              className="description_titles"
+            />{" "}
+          </b>
         </p>
         <div className="card">
           <button onClick={() => (window.location.href = oldPort)}>
