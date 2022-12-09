3c7cd232c833a99c827788a0ab26cea0b2d69afa
sdrrv <duartecelvas@gmail.com>
2022-12-09 22:44:38 +0000

    Size and bold

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import "./App.scss";
 
 function App() {
   const oldPort = "https://web.tecnico.ulisboa.pt/duartecelvas/";
+  const titles = ["Software Engineer", "Developer"];
 
   return (
     <>
       <ParticleBackground />
       <div className="App">
         <h1>Duarte Elvas</h1>
-        <p className="read-the-docs">
-          I am a <ReactTypingEffect text={["Software Engineer", "Developer"]} />
+        <p className="description">
+          I am a{" "}
+          <b>
+            {" "}
+            <ReactTypingEffect
+              text={titles}
+              className="description_titles"
+            />{" "}
+          </b>
         </p>
         <div className="card">
           <button onClick={() => (window.location.href = oldPort)}>
