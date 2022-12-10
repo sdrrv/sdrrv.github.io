992fa90c740e2b0fd0859e5a1957a75ae3602202
sdrrv <duartecelvas@gmail.com>
2022-12-10 13:00:40 +0000

    Button props added

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Button.component.scss";
 
+type buttonType = "normal" | "circle";
+
 type ButtonProps = {
   children?: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+  buttonType?: buttonType;
 };
 
-function Button({ children }: ButtonProps) {
+function Button({ children, className, onClick }: ButtonProps) {
   return (
-    <div className="card">
-      <button> {children} </button>
+    <div className={"card " + className}>
+      <button type="button" onClick={onClick}>
+        {children}
+      </button>
     </div>
   );
 }
