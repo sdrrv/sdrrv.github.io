8abd3a7023bbaf4dd583c170437b3a54b2e99e71
sdrrv <duartecelvas@gmail.com>
2022-12-13 19:57:52 +0000

    Rotation animation added

--- src/Components/PopupMenu/PopupMenu.tsx
+++ src/Components/PopupMenu/PopupMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import "./PopupMenu.component.scss";
 import { motion, useCycle } from "framer-motion";
 import Button from "../Button/Button";
@@ -7,9 +7,21 @@ type props = {
   className?: string;
 };
 
+const iconVariants = {
+  open: {
+    rotate: 90,
+    center: true,
+    color: "#242424",
+  },
+  closed: {
+    rotate: 0,
+    center: true,
+    color: "rgba(255, 255, 255, 0.87)",
+  },
+};
+
 function PopupMenu({ className }: props) {
-  const [isOpen, toggleOpen] = useCycle(false, true);
-  const containerRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <motion.div
       initial={{ opacity: 0, x: -50 }}
@@ -17,16 +29,18 @@ function PopupMenu({ className }: props) {
       transition={{ delay: 1.6, duration: 1, type: "tween" }}
       className="container"
     >
-      <motion.nav
-        className={className}
-        initial={false}
-        animate={isOpen ? "open" : "closed"}
-        ref={containerRef}
+      <Button
+        onClick={() => setIsOpen(!isOpen)}
+        buttonType="empty"
+        className="menu"
+        animationProps={{
+          animate: isOpen ? "open" : "closed",
+          variants: iconVariants,
+          transition: {},
+        }}
       >
-        <Button buttonType="circle" className="menu">
-          <i className="bi-list" />
-        </Button>
-      </motion.nav>
+        <i className={isOpen ? "bi-x" : "bi-list"} />
+      </Button>
     </motion.div>
   );
 }
