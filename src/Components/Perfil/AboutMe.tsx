992dc2490cd9fd59baaa0f8bdc4ce18e423ad426
Duarte Elvas <duartecelvas@gmail.com>
2022-12-26 12:00:27 +0000

    Image added

--- src/Components/Perfil/AboutMe.tsx
+++ src/Components/Perfil/AboutMe.tsx
@@ -1,10 +1,14 @@
 import "./AboutMe.component.scss";
+import selfImage from "../../assets/self1.png";
 
 function AboutMe() {
   return (
     <section>
-      <div className="about_me">
-        <h1>Hello World</h1>
+      <div className="about-me">
+        <div className="about-me_text">
+          <h2>About me</h2>
+        </div>
+        <img className="about-me_image" src={selfImage} alt="It's me!" />
       </div>
     </section>
   );
