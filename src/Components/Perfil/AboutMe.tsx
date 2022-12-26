c94697bddcc9b0ef885a2f08a11357c741f20a09
Duarte Elvas <duartecelvas@gmail.com>
2022-12-26 12:20:19 +0000

    Text added

--- src/Components/Perfil/AboutMe.tsx
+++ src/Components/Perfil/AboutMe.tsx
@@ -1,5 +1,6 @@
 import "./AboutMe.component.scss";
 import selfImage from "../../assets/self1.png";
+import { calculateAge } from "../../scripts/ageCalculator";
 
 function AboutMe() {
   return (
@@ -9,7 +10,17 @@ function AboutMe() {
           <h2>
             About me <i className="bi-file-person-fill" />
           </h2>
+          <p>
+            My name is <b>Duarte Elvas</b>, I am a{" "}
+            <b>{calculateAge(new Date("2000-10-24"))}-year-old</b> student
+            living in <b>Portugal</b>, and studying Software Engineering &
+            Computer Science at{" "}
+            <a href="https://tecnico.ulisboa.pt/en/">
+              Instituto Superior Técnico.
+            </a>
+          </p>
         </div>
+
         <img className="about-me_image" src={selfImage} alt="It's me!" />
       </div>
     </section>
--- src/scripts/ageCalculator.ts
+++ src/scripts/ageCalculator.ts
@@ -1,11 +1,11 @@
 function calculateAge(birthday: Date): number {
-    const today = new Date();
-    let age = today.getFullYear() - birthday.getFullYear();
-    const month = today.getMonth() - birthday.getMonth();
-    if (month < 0 || (month === 0 && today.getDate() < birthday.getDate())) {
-        age--;
-    }
-    return age;
+  const today = new Date();
+  let age = today.getFullYear() - birthday.getFullYear();
+  const month = today.getMonth() - birthday.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < birthday.getDate())) {
+    age--;
+  }
+  return age;
 }
 
-export {calculateAge}
+export { calculateAge };
