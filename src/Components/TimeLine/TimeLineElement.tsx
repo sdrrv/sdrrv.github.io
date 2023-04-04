d7a08b6a7a3def9b43ab492988472714cac67b2d
sdrrv <duartecelvas@gmail.com>
2023-04-04 22:50:34 +0100

    Animations

--- src/Components/TimeLine/TimeLineElement.tsx
+++ src/Components/TimeLine/TimeLineElement.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 import "./TimeLine.component.scss";
 
@@ -15,15 +16,22 @@ function TimeLineElement({
   iconNode,
 }: TimeLineElementProps) {
   return (
-    <div className="timeline_icon">
-      {iconNode}
+    <motion.div className="timeline_icon">
+      <motion.div
+        className="timeline_icon_icon"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 1 }}
+        transition={{ duration: 0.1 }}
+      >
+        {iconNode}
+      </motion.div>
       <div className={"timeline_icon_date " + (reversed ? "left" : "right")}>
         {dateNode}
       </div>
       <div className={"timeline_icon_content " + (reversed ? "right" : "left")}>
         {children}
       </div>
-    </div>
+    </motion.div>
   );
 }
 export default TimeLineElement;
